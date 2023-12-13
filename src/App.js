8cb011cfcea4bcd8752d0969fcaddb5cb71e3786
cheun <cheungws317@gmail.com>
2023-12-14 06:01:13 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to the home page!</p>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>This is the about page.</p>
+  </div>
+);
+
+const Services = () => (
+  <div>
+    <h2>Services</h2>
+    <p>Here are our services.</p>
+  </div>
+);
+
+const Contact = () => (
+  <div>
+    <h2>Contact</h2>
+    <p>Contact us here.</p>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul className="navbar">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
